23b4ee5a298afda4947a64a56c86bf0e6257d918
FaultyFaulty-glitch <faultywindows+github@gmail.com>
2024-07-23 11:14:00 +0100

    Add Source

---
+++ source/states/OutdatedState.hx
@@ -0,0 +1,51 @@
+package states;
+
+class OutdatedState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var warnText:FlxText;
+	override function create()
+	{
+		super.create();
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Hello there! Seems like you are runing an   \n
+			outdated version of Average PC Engine (" + MainMenuState.psychEngineVersion + "),\n
+			please update to " + TitleState.updateVersion + "!\n
+			Press ESCAPE to proceed anyway. and not enjoy the new features :(\n
+			\n
+			Thank you for using Average PC Engine!",
+			32);
+		warnText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		add(warnText);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			if (controls.ACCEPT) {
+				leftState = true;
+				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
+			}
+			else if(controls.BACK) {
+				leftState = true;
+			}
+
+			if(leftState)
+			{
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				FlxTween.tween(warnText, {alpha: 0}, 1, {
+					onComplete: function (twn:FlxTween) {
+						MusicBeatState.switchState(new MainMenuState());
+					}
+				});
+			}
+		}
+		super.update(elapsed);
+	}
+}
